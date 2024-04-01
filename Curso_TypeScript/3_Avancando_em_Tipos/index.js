791b6bb94e61e68accc09111486e896355c7cb00
Bernardo P Zapelini <94933673+bino1432@users.noreply.github.com>
2024-04-01 19:52:06 +0000

    Curso_Tsc

---
+++ Curso_TypeScript/3_Avancando_em_Tipos/index.js
@@ -0,0 +1,12 @@
+// 1 - Arrays
+var numbers = [1, 2, 3];
+numbers.push(5);
+console.log(numbers[2]);
+var strings = ["Bernardo", "Zapelini"];
+console.log(strings[0]);
+// 2 - Outra sintaxe Array
+var numeros = [100, 200];
+numeros.push(300);
+console.log(numbers[2]);
+//3 - Any
+var qualquerCoisa = [1, "Bernardo", true];
---
+++ Curso_TypeScript/3_Avancando_em_Tipos/index.ts
@@ -0,0 +1,22 @@
+// 1 - Arrays
+let numbers:number[] = [1, 2, 3]
+
+numbers.push(5)
+
+console.log(numbers[2])
+
+let strings:String[] = ["Bernardo", "Zapelini"]
+
+console.log(strings[0])
+
+// 2 - Outra sintaxe Array
+
+const numeros: Array<number> = [100, 200]
+
+numeros.push(300)
+
+console.log(numbers[2])
+
+//3 - Any
+
+const qualquerCoisa:any = [1, "Bernardo", true]
